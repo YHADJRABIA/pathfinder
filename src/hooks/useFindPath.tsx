c53d8d50848b114d7350e0fcdd1db5569b562899
YHR <h_yacine@live.fr>
2024-09-21 00:51:54 +0200

    Handle pathfinding in backend

--- src/hooks/useFindPath.tsx
+++ src/hooks/useFindPath.tsx
@@ -1,14 +1,34 @@
 import { useState } from "react"
-import { findShortestPath } from "@/utils/pathfinder"
+import axios from "axios"
 import { Coordinate, Grid } from "@/types/pathfinder"
+import { getErrorMessage } from "@/utils/error"
 
 export function useFindPath() {
   const [path, setPath] = useState<Coordinate[] | null | undefined>(undefined)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
 
-  const findPathFromData = (grid: Grid, start: Coordinate, end: Coordinate) => {
-    const foundPath = findShortestPath(grid, start, end)
-    setPath(foundPath)
+  const findPathFromData = async (
+    grid: Grid,
+    start: Coordinate,
+    end: Coordinate
+  ) => {
+    setIsLoading(true)
+    setError(null)
+    try {
+      const res = await axios.post("/api/find-path", { grid, start, end })
+
+      if (res.data.success) {
+        setPath(res.data.path)
+      } else {
+        setError(res.data.message)
+      }
+    } catch (err) {
+      setError(getErrorMessage(err))
+    } finally {
+      setIsLoading(false)
+    }
   }
 
-  return { path, findPathFromData }
+  return { path, findPathFromData, isLoading, error }
 }
